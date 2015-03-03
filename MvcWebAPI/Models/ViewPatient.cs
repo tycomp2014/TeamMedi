c0cce00dfd6afec0927a8563c483caa97af42ec6
Craig Gallagher <S00133386@mail.itsligo.ie>
2015-03-03 10:42:46 +0000

    First commit

--- MvcWebAPI/Models/ViewPatient.cs
+++ MvcWebAPI/Models/ViewPatient.cs
@@ -10,7 +10,9 @@
 namespace MvcWebAPI.Models
 {
     using System;
-    using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
     
     public partial class ViewPatient
     {
@@ -19,9 +21,11 @@ namespace MvcWebAPI.Models
         public int GPID { get; set; }
         public string Firstname { get; set; }
         public string Surname { get; set; }
+        [DataType(DataType.Date),
+        DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public System.DateTime DOB { get; set; }
         public string Medication { get; set; }
-        public string MedicationInfo { get; set; }
         public string Alergies { get; set; }
+        public string MedicationHistory { get; set; }
     }
 }
